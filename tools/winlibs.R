2b1bf1fb369b689c9a9d0c1b71f37278de2e9781
polmine <polmine@POLMINE.DE>
2017-12-21 08:00:01 +0100

    i386 working again

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,12 +1,42 @@
 # Build against libxml2 from Rtools
 if (TRUE) {
-  # dir.create("../windows", showWarnings = FALSE)
-  # dir.create("../inst/cwb", showWarnings = FALSE)
-  # dir.create("../inst/libs", showWarnings = FALSE)
-  # dir.create("../inst/libs/x64", showWarnings = FALSE)
+  dir.create("../inst/cwb", showWarnings = FALSE)
+  dir.create("../inst/libs", showWarnings = FALSE)
+  dir.create("../inst/libs/x64", showWarnings = FALSE)
+  dir.create("../inst/libs/i386", showWarnings = FALSE)
  
   if(getRversion() < "3.3.0") setInternet2()
  
+  
+  ## i386 - files ----------------------
+
+  download.file(
+    "http://polmine.sowi.uni-due.de/public/dlls/i386/libcl.a",
+    "../inst/libs/i386/libcl.a", quiet = TRUE,
+    method = "curl"
+  )
+  download.file(
+    "http://polmine.sowi.uni-due.de/public/dlls/i386/libglib-2.0-0.dll", 
+    "../inst/libs/i386/libglib-2.0-0.dll", quiet =  TRUE,
+    method = "curl"
+  )
+  download.file(
+    "http://polmine.sowi.uni-due.de/public/dlls/i386/libpcre-0.dll",
+    "../inst/libs/i386/libpcre-0.dll", quiet = TRUE,
+    method = "curl"
+  )
+  download.file(
+    "http://polmine.sowi.uni-due.de/public/dlls/i386/libpcre-1.dll", 
+    "../inst/libs/i386/libpcre-1.dll", quiet = TRUE,
+    method = "curl"
+  )
+  download.file("http://polmine.sowi.uni-due.de/public/dlls/i386/libpcreposix-0.dll", 
+    "../inst/libs/i386/libpcreposix-0.dll", quiet = TRUE,
+    method = "curl"
+  )
+
+
+  ## x64 - files -----------------------
   download.file(
     "http://polmine.sowi.uni-due.de/public/dlls/x86/libglib-2.0-0.dll",
     "../inst/libs/x64/libglib-2.0-0.dll", quiet = TRUE,
